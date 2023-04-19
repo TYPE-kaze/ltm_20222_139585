a867c0192da5f1b8047556473898c331013799aa
kaze <98795754+TYPE-kaze@users.noreply.github.com>
2023-04-19 10:09:17 +0700

    update info_sever and info_client homeworks

--- homeworks/onClassAssignment/info_client.c
+++ homeworks/onClassAssignment/info_client.c
@@ -1,118 +1,68 @@
 #include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
 #include <sys/types.h>
-#include <netdb.h>
-#include <arpa/inet.h>
+#include <sys/socket.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <netdb.h>
+#include <string.h>
+#include <arpa/inet.h>
 
-#define MAX_LEN 256
+int main() {
+    int client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-struct SinhVien {
-    char mssv[MAX_LEN];
-    char hoTen[MAX_LEN];
-    char ngaySinh[MAX_LEN];
-    float diemTB;
-};
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(9000); 
 
-int main(int argc, char *argv[])
-{
-    // if (argc != 3)
-    // {
-    //     printf("Sai tham so.\n");
-    //     return 1;
-    // }
-
-    // char const *destIP = argv[1];
-    // int const destPort = atoi(argv[2]);
-
-
-    // int client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-    // struct sockaddr_in addr;
-    // addr.sin_family = AF_INET;
-    // addr.sin_addr.s_addr = inet_addr(destIP);
-    // addr.sin_port = htons(destPort);
-
-    // int ret = connect(client, (struct sockaddr *)&addr, sizeof(addr));
-    // if (ret == -1)
-    // {
-    //     perror("connect() failed");
-    //     return 1;
-    // }
-
-    //Nhap du lieu va tao buffer chua du lieu can du
-    char name[64];
-    int num_of_disk;
+    if (connect(client, (struct sockaddr *)&addr, sizeof(addr))) {
+        perror("connect() failed");
+        return 1;
+    }
+        
+    // Nhap du lieu va tao buffer chua du lieu can gui
+    //Example : MY_LAPTOP_DELL CxxDyyEzz
     char buf[256];
-    struct DiskInfo {
-        char name[1];
-        unsigned short volume;
-    };
+    int pos = 0;
 
-    printf("Nhap ten may tinh:\n");
-    fgets(name, sizeof(name), stdin);
-    name[strlen(name) - 1] = 0;
+    char computer_name[64];
+    printf("Nhap ten may tinh: ");
+    scanf("%s", computer_name);
 
-    printf("Nhap so o dia:\n");
-    scanf("%d", &num_of_disk);
+    strcpy(buf, computer_name);
+    pos += strlen(computer_name);
+    buf[pos] = 0;
+    pos++;
+
+    int num_drives;
+    printf("Nhap so o dia: ");
+    scanf("%d", &num_drives);
     getchar();
 
-    //Get disk info
-    struct DiskInfo disks[num_of_disk];
+    char drive_letter;
+    unsigned short drive_size;
 
-    // for (int i = 0; i < num_of_disk; i++) {
-    //     printf("Nhap ten ky tu o dia %d:" ,i);
-    //     scanf("%s", &disks[i].name);
-    //     getchar();
+    for (int i = 0; i < num_drives; i++)
+    {
+        printf("Nhap ky tu: ");
+        scanf("%c", &drive_letter);
 
-    //     printf("Nhap dung luong o dia %s (GB):", disks[i].name);
-    //     scanf("%d", &disks[i].volume);
-    //     getchar();
-    //     printf("Disk Letter:%s - Volume:%d GB\n", disks[i].name, disks[i].volume);
-    // }
-    for (int i = 0; i < num_of_disk; i++) {
-        printf("Nhap ten ky tu o dia %d:" ,i);
-        scanf("%s", &disks[i].name);
+        buf[pos] = drive_letter;
+        pos++;
+
+        printf("Nhap kich thuoc: ");
+        scanf("%hu", &drive_size);
         getchar();
 
-        printf("Nhap dung luong o dia %s (GB):", disks[i].name);
-        scanf("%d", &disks[i].volume);
-        getchar();
-        printf("Disk Letter:%s - Volume:%d GB\n", disks[i].name, disks[i].volume);
+        memcpy(buf + pos, &drive_size, sizeof(drive_size));
+        pos += sizeof(drive_size);
     }
 
+    printf("Buffer size: %d\n", pos);
 
+    send(client, buf, pos, 0);
 
-    // sprintf(buf,"%s %s %s %.2f", mssv, hoten, ngaySinh, dtb);
+    // Ket thuc, dong socket
+    close(client);
 
-
-    // while (1)
-    // {
-    //     printf("Nhap thong tin sinh vien: \n");
-    //     printf("Nhap MSSV:\n");
-    //     scanf("%s", mssv);
-    //     getchar();
-
-    //     printf("Nhap ho ten:\n");
-    //     fgets(hoten, sizeof(hoten), stdin);
-    //     hoten[strlen(hoten) - 1] = 0;
-
-    //     printf("Nhap ngay sinh:\n");
-    //     scanf("%s", ngaySinh);
-    //     getchar();
-
-    //     printf("Nhap Diem TB:\n");
-    //     scanf("%f", &dtb);
-    //     getchar();
-
-    //     sprintf(buf,"%s %s %s %.2f", mssv, hoten, ngaySinh, dtb);
-    //     printf("%s\n", buf);
-
-    //     send(client, buf, strlen(buf), 0);
-
-    // }
-    
     return 0;
 }
